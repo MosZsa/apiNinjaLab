a4b0c171fed57627f8fef8097bcdd1f6b1daf7a9
Jack_ga <162978908+MosZsa@users.noreply.github.com>
2025-06-08 21:15:57 +0900

    Update auth.py
    up name for func authorization

--- nutrition/auth.py
+++ nutrition/auth.py
@@ -14,7 +14,7 @@ class LoginSchema(BaseModel):
     username: str
     password: str
 
-@auth_router.post("register", tags=["Аутентификация"])
+@auth_router.post("register", tags=["Аутентификация"], summary="Регистрация")
 def register_user(request, data: RegisterSchema):
     if User.objects.filter(username=data.username).exists():
         return {"error": "Пользователь с таким именем уже существует"}
@@ -26,7 +26,7 @@ def register_user(request, data: RegisterSchema):
     return {"success": True, "user_id": user.id}
 
 
-@auth_router.post("login", tags=["Аутентификация"])
+@auth_router.post("login", tags=["Аутентификация"], summary="Войти")
 def login_user(request, data: LoginSchema):
     user = authenticate(request, username=data.username, password=data.password)
     if user is not None:
@@ -34,13 +34,13 @@ def login_user(request, data: LoginSchema):
         return {"success": True}
     return {"error": "Неверный логин или пароль"}
 
-@auth_router.post("logout", tags=["Аутентификация"])
+@auth_router.post("logout", tags=["Аутентификация"], summary="Выйти")
 def logout_user(request):
     logout(request)
     return {"success": True}
 
 
-@auth_router.get("me", tags=["Аутентификация"])
+@auth_router.get("me", tags=["Аутентификация"], summary="Вывод имени работающего на данный момент")
 def get_current_user(request):
     if request.user.is_authenticated:
         return {"username": request.user.username}
